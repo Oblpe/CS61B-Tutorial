d963452eb02f077d78a488a24c82e34210268e9e
yuxi <1321541076@qq.com>
2023-03-15 20:44:03 +0800

    第一次提交

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,7 +2,12 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
-
+    /** determin if the given year is a leap year
+     *  @param year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
